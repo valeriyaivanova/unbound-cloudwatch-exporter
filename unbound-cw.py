5e7be230660c70af5d8d338e0ea07fa55fee4c9c
valia <valeriya.ivanova@technofy.io>
2018-04-17 10:57:36 +0300

    Make small modification in terms to wotk with unbound app in the same instance. Change the Unbound binary dir.

--- unbound-cw.py
+++ unbound-cw.py
@@ -22,19 +22,27 @@ aws_namespace = "MyNamespace/Unbound"
 # Map Unbound individual stats to AWS CloudWatch metrics
 aws_metrics_mapping = {
     "total.num.queries": "NumQueries",
+    "total.num.queries_ip_ratelimited": "NumQueriesIPRateLimited",
     "total.num.cachehits": "NumCacheHits",
     "total.num.cachemiss": "NumCacheMiss",
     "total.num.prefetch": "NumPrefetch",
     "total.num.zero_ttl": "NumZeroTTL",
     "total.num.recursivereplies": "RecursiveReplies",
     "total.requestlist.avg": "RequestListsAverage",
+    "total.requestlist.max": "RequestListsMax",
     "total.requestlist.overwritten": "RequestListsOverwritten",
     "total.requestlist.exceeded": "RequestListsExceeded",
+    "total.requestlist.current.all": "RequestListsCurrentAll",
+    "total.requestlist.current.user": "RequestListsCurrentUser",
     "total.recursion.time.avg": "RecursionTimeAverage",
+    "total.recursion.time.median": "RecursionTimeMedian",
     "total.tcpusage": "TcpUsage",
+    "time.now": "TimeNow",
+    "time.up": "TimeUp",
+    "time.elapsed": "TimeElapsed"
 }
 # Unbound binaries path
-ub_path = "/usr/local/sbin"
+ub_path = "/usr/sbin"
 
 
 ###################
